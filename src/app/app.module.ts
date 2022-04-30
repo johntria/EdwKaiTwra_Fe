92ab0e70e5dc48de424103c6141bbe507ae97d18
John Triantafyllakis <john.3fyl@gmail.com>
2022-04-30 22:22:14 +0300

    removed comment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { LoggerModule, NgxLoggerLevel } from "ngx-logger";
-// HttpClientModule is only needed if you want to log on server or if you want to inspect sourcemaps
 import { HttpClientModule } from "@angular/common/http";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
