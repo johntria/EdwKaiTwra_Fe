459d232353953a9de6461d888a30d093805f5a23
John Triantafyllakis <john.3fyl@gmail.com>
2022-05-16 23:40:28 +0300

    Shared Logo implemented

---
+++ src/shared/modules/shared.module.ts
@@ -0,0 +1,17 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { NgPrimeModule } from "./ng-prime/ng-prime.module";
+
+@NgModule({
+  declarations: [
+
+  ],
+  imports: [
+    CommonModule,
+    NgPrimeModule,
+  ],
+  exports: [
+
+  ]
+})
+export class SharedModule { }
